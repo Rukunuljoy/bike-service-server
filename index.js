1e0dc1b8345f0df2cf329c11926c8ac9fd093bb9
Rukunuljoy <rukunuljoypub58@gmail.com>
2022-11-26 09:13:20 +0600

    database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, CURSOR_FLAGS, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -13,6 +13,49 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hvxqvqc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const bikeCollection = client.db('royalEnfield').collection('royalBikes')
+        const suzukiCollection = client.db('suzuki').collection('suzukiBikes')
+
+
+        app.get('/royalBikes',async(req,res)=>{
+            const query = {}
+            const cursor = bikeCollection.find(query)
+            const royalBikes = await cursor.toArray();
+            res.send(royalBikes);
+        })
+        
+        app.get('/royalBikes/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const royal = await bikeCollection.findOne(query);
+            res.send(royal)
+        })
+
+        
+        app.get('/suzukiBikes',async(req,res)=>{
+            const query = {}
+            const cursor = suzukiCollection.find(query)
+            const suzukiBikes = await cursor.toArray();
+            res.send(suzukiBikes);
+        })
+        
+        app.get('/suzukiBikes/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id:ObjectId(id)};
+            const suzuki = await suzukiCollection.findOne(query);
+            res.send(suzuki)
+        })
+
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(err=>console.error(err))
 
 
 app.get('/',(req,res)=>{
