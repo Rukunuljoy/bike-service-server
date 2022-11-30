bcba302472a41978f69e5e114625114e5cbefc3a
Rukunuljoy <rukunuljoypub58@gmail.com>
2022-11-30 18:40:00 +0600

    simple adder

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ async function run() {
     const usersCollection = client.db("bikes").collection("users");
     const suzukiCollection = client.db("bikes").collection("suzukiBike");
     const yamahaCollection = client.db("bikes").collection("yamahaBike");
+    const paymentsCollection = client.db('autoBuzz').collection('payments');
 
     app.get("/royalBikes", async (req, res) => {
       const query = {};
